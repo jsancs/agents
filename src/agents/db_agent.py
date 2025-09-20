f6414b38149569e2d72e1a028f8fa024b5b92160
jsancs <javivisan19@gmail.com>
2025-09-20 15:05:09 +0200

    Tool approval process

--- src/agents/db_agent.py
+++ src/agents/db_agent.py
@@ -13,8 +13,8 @@ def build_agent() -> Agent:
     def read_row(id: str) -> None:
         return f"Row with id {id}: Name -> Agent"
 
-    # @db_agent.tool_plain(requires_approval=True)
-    # def delete_row(id: str) -> None:
-    #     return f"Row with id {id} deleted"
+    @db_agent.tool_plain(requires_approval=True)
+    def delete_row(id: str) -> None:
+        return f"Row with id {id} deleted"
 
     return db_agent
--- src/graph.py
+++ src/graph.py
@@ -1,7 +1,8 @@
 import asyncio
 from dataclasses import dataclass
 
-from pydantic_ai import Agent
+from pydantic_ai import Agent, DeferredToolRequests, DeferredToolResults, ToolDenied
+from pydantic_ai.tools import DeferredToolResult
 from pydantic_graph import BaseNode, End, Graph, GraphRunContext
 from dotenv import load_dotenv
 from .models.agent_enum import AgentEnum
@@ -39,7 +40,31 @@ class DBManagerNode(BaseNode[None, None, str]):
         result = await ctx.deps.db_agent.run(ctx.deps.query)
         # print(result.output)
 
+        if isinstance(result.output, DeferredToolRequests):
+            tool_app = self._handle_tool_approval(result.output)
+            result = await ctx.deps.db_agent.run(
+                message_history=result.all_messages(), deferred_tool_results=tool_app
+            )
+
         return End(result.output)
+
+    def _handle_tool_approval(
+        self, requests: DeferredToolRequests
+    ) -> DeferredToolResult:
+        results = DeferredToolResults()
+
+        for call in requests.approvals:
+            result = False
+            if call.tool_name == "delete_row":
+                # Approve action
+                # TODO: This is just an example, you should implement here your custom tool approval logic
+                result = True
+            else:
+                # deny all other tools
+                result = ToolDenied("Deleting files is not allowed")
+
+            results.approvals[call.tool_call_id] = result
+        return results
 
 
 @dataclass
